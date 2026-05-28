dfc614905225765679cb220d07defa74b1990777
JasonDoe <43284628+Thafoxes@users.noreply.github.com>
2026-05-29 01:27:11 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,7 @@ const router = createRouter({
     },
     {
       path: '/manage-schedule',
-      name: 'manage-schedule',
+      name: 'manage_schedule',
       component: () => import('../views/ManageScheduleView.vue'),
       meta: { requiresAuth: true }
     }
