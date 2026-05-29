a30a356081859a1f2fc22e4c4161aacb372cfdc6
JasonDoe <43284628+Thafoxes@users.noreply.github.com>
2026-05-30 03:11:26 +0800

    Update ViewCalendarComponent.vue

--- src/components/ViewCalendarComponent.vue
+++ src/components/ViewCalendarComponent.vue
@@ -105,9 +105,17 @@
       </div>
 
       <!-- Action Buttons -->
+       
       <div class="p-4 flex flex-col gap-2 mt-auto border-t border-[#d6d68b]">
         <router-link to="/add-time-table" class="w-full bg-[#5C001E] hover:bg-[#4a0018] text-white font-bold py-3 rounded-md shadow text-xs transition-colors flex justify-center items-center gap-2">
           <span>+</span> Add new time table
+        </router-link>
+        <router-link to="/edit-time-table" class="w-full bg-[#5C001E] hover:bg-[#4a0018] text-white font-bold py-3 rounded-md shadow text-xs transition-colors flex justify-center items-center gap-2">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+               <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+            </svg>
+            </span> Edit time table
         </router-link>
       </div>
     </aside>
